2d4d56e2a97df7119ab7571aafc838b9d2849d55
Brown <b9602104@gmail.com>
2017-10-04 13:51:35 +0800

    commit

--- app.js
+++ app.js
@@ -26,20 +26,48 @@ app.get('/', function(req, res){
         var $ = cheerio.load(b);
         var result = [];
         var titles = $(".list p");
-        var imgs = $(".list img");   
+        var imgs = $(".list img"); 
+        var times = $(".list span");        
         var urls = $(".list a");
         for(var i=0;i<titles.length;i++) {
             //var obj = {};
             obj = {};
-            obj['title'] = $(titles[i]).text().trim();
-            obj['img'] = $(imgs[i]).attr('src');
-            obj['url'] = $(urls[i]).attr('href');
+            obj['heading'] = $(titles[i]).text().trim();
+            obj['imageUrl'] = $(imgs[i]).attr('src');
+            obj['publishedDate'] = $(times[i]).text().trim();            
+            obj['contentUrl'] = $(urls[i]).attr('href');
             result.push(obj);
         }       
         res.send(JSON.stringify(result));        
       });
 
 })
+
+//路徑 (ex : 127.0.0.1:3000/Detail)
+app.get('/Detail',function(req,res){
+    
+        if (req.query['url']) {       
+            
+            request({
+                url: req.query['url'],
+                method: "GET"
+              }, function(e,r,b) {
+                if(e || !b) { return; }
+                var $ = cheerio.load(b);
+                var result = [];   
+                var contents = $(".content p").text().trim();
+                result.push(contents);
+                res.send(JSON.stringify(result));                                                    
+              });
+    
+        } else {
+            var result = []; 
+            result.push("URL Not Find");
+            res.send(JSON.stringify(result));                                                    
+        }
+        
+    })
+    
 
 //Web 監聽的 Port 
 app.listen(PORT, function(){
